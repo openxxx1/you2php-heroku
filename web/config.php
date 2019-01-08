d3b4b44c6f58fef6c32289df679e866ca981aa5a
openxxx1 <46500966+openxxx1@users.noreply.github.com>
2019-01-09 06:04:03 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyBG5Lqdx0mtxteTiZWGZDMKzUYwsi9IOos');//这里改成您的api key
+define('GJ_CODE', 'TW');//国家代码
+define('SITE_NAME', 'zoro看视频');//网站名字
+define('TITLENAME', 'zoro看视频');//网站名字
+define('EN2DEKEY', 'AnQUP1XmEF435e67CtzDSh6');//一个随机字符串，加解密KEY
+define('EMAIL', 'openxxx.xxxopen@gmail.com');//邮箱
 ?>
